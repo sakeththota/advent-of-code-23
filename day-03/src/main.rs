bfce266555c8a9db2a160e611f389184c69df83c
Saketh Thota <saketht@umich.edu>
2023-12-05 16:51:40 -0600

    done w day-03 p1

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -18,32 +18,45 @@ fn read_grid(input: &str) -> Result<Vec<Vec<char>>> {
     Ok(grid)
 }
 
+// returns true if the cell adjacent or diagonal to i,j is not a number and not a dot and false
+// otherwise elegantly in rust
 fn is_part_number(grid: &Vec<Vec<char>>, i: usize, j: usize) -> Result<bool> {
-    if i > 0 && grid[i - 1][j] != '.'
-        || i < grid.len() - 1 && grid[i + 1][j] != '.'
-        || j > 0 && grid[i][j - 1] != '.'
-        || j < grid[0].len() - 1 && grid[i][j + 1] != '.'
-        || i > 0 && j > 0 && grid[i - 1][j - 1] != '.'
-        || i > 0 && j < grid[0].len() - 1 && grid[i - 1][j + 1] != '.'
-        || i < grid.len() - 1 && j > 0 && grid[i + 1][j - 1] != '.'
-        || i < grid.len() - 1 && j < grid[0].len() - 1 && grid[i + 1][j + 1] != '.'
-    {
-        Ok(true)
-    } else {
-        Ok(false)
+    let offsets = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1), /*(i, j)*/ (0, 1),
+        (1, -1), (1, 0), (1, 1),
+    ];
+
+    for &(di, dj) in &offsets {
+        let ni = i as i32 + di;
+        let nj = j as i32 + dj;
+
+        if ni >= 0 && nj >= 0 &&
+           ni < grid.len() as i32 && nj < grid[0].len() as i32 &&
+           !grid[ni as usize][nj as usize].is_digit(10) &&
+           grid[ni as usize][nj as usize] != '.' {
+            return Ok(true);
+        }
     }
+
+    Ok(false)
+
 }
 
 fn part1(input: &str) -> Result<usize> {
     let grid = read_grid(input)?;
     let mut sum: usize = 0;
+    let mut current_number = String::from("");
+    let mut check = false;
     for i in 0..grid.len() {
         for j in 0..grid[0].len() {
-            println!("{} {} {}", i, j, grid[i][j]);
-            if grid[i][j].is_digit(10) && is_part_number(&grid, i, j)? {
-                let val = grid[i][j].to_digit(10).unwrap() as usize;
-                println!("{} {} {}", i, j, val);
-                sum += val;
+            if grid[i][j].is_digit(10) {
+                check = if is_part_number(&grid,i,j)? {true} else {check};
+                current_number.push(grid[i][j]);
+            } else {
+                if check { sum += current_number.parse::<usize>()?; }
+                current_number = String::from("");
+                check = false;
             }
         }
     }
