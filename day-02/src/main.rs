b3329ad22fc870cf4097cc85dd341dd86b73e653
Saketh Thota <sakeththota01@gmail.com>
2023-12-04 19:56:25 -0600

    completed day-02

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -8,12 +8,51 @@ fn main() {
     dbg!(p2);
 }
 
-fn part1(input: &str) -> Result<()> {
-    Ok(())
+fn part1(input: &str) -> Result<usize> {
+    let mut sum: usize = 0;
+    'game_loop: for (id, line) in input.lines().enumerate() {
+        let game = line[line.find(':').unwrap()+2..].split("; ");
+        for draw in game {
+            for pair in draw.split(", ") {
+                let (num, color) = pair.split_once(" ").unwrap();
+                let num: usize = num.parse::<usize>().unwrap();
+                let is_valid = match color {
+                    "red" => num <= 12,
+                    "green" => num <= 13,
+                    "blue" => num <= 14,
+                    _ => panic!("should never reach here"),
+                };
+                if !is_valid { continue 'game_loop; }
+            }
+        }
+        sum += id + 1;
+    }
+
+    Ok(sum)
 }
 
-fn part2(input: &str) -> Result<()> {
-    Ok(())
+fn part2(input: &str) -> Result<usize> {
+    let mut sum: usize = 0;
+    for (id, line) in input.lines().enumerate() {
+        let game = line[line.find(':').unwrap()+2..].split("; ");
+        let (mut max_r, mut max_g, mut max_b) = (0,0,0);
+        for draw in game {
+            for pair in draw.split(", ") {
+                let (num, color) = pair.split_once(" ").unwrap();
+                let num: usize = num.parse::<usize>().unwrap();
+                match color {
+                    "red" => if num > max_r { max_r = num; },
+                    "green" => if num > max_g { max_g = num; },
+                    "blue" => if num > max_b { max_b = num; },
+                    _ => panic!("should never reach here"),
+                };
+            }
+        }
+        let power = max_r*max_g*max_b;
+        sum += power
+    }
+
+    Ok(sum)
 }
 
 #[cfg(test)]
@@ -22,12 +61,18 @@ mod tests {
 
     #[test]
     fn part1_ex() {
-        assert_eq!(true, true);
+        let expected = 8;
+        let result = part1(include_str!("part1_ex.in"));
+        println!("expected: {:?}, received {:?}", expected, result);
+        assert_eq!(result.is_ok_and(|x| x == expected), true);
     }
 
     #[test]
     fn part2_ex() {
-        assert_eq!(true, true);
+        let expected = 2286;
+        let result = part2(include_str!("part2_ex.in"));
+        println!("expected: {:?}, received {:?}", expected, result);
+        assert_eq!(result.is_ok_and(|x| x == expected), true);
     }
 }
 
