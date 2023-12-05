e683d8c48c1f19b7f96e49f24f536516f5330a5e
Saketh Thota <saketht@umich.edu>
2023-12-04 18:45:55 -0600

    setup for day-02

---
+++ day-02/src/main.rs
@@ -0,0 +1,33 @@
+use::anyhow::Result;
+
+fn main() {
+    let input = include_str!("input.in");
+    let p1 = part1(input);
+    dbg!(p1);
+    let p2 = part2(input);
+    dbg!(p2);
+}
+
+fn part1(input: &str) -> Result<()> {
+    Ok(())
+}
+
+fn part2(input: &str) -> Result<()> {
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_ex() {
+        assert_eq!(true, true);
+    }
+
+    #[test]
+    fn part2_ex() {
+        assert_eq!(true, true);
+    }
+}
+
