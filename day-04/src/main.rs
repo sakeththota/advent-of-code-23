f07fcc656eb843ae2d683f01ba95a66e6fca601d
Saketh Thota <sakeththota01@gmail.com>
2023-12-06 15:45:41 -0600

    covering dbg cases

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,27 +1,34 @@
-use::anyhow::Result;
+use ::anyhow::Result;
 
 fn main() {
     let input = include_str!("input.in");
     let p1 = part1(input);
-    dbg!(p1);
+    dbg!(p1.unwrap());
     let p2 = part2(input);
-    dbg!(p2);
+    dbg!(p2.unwrap());
 }
 
-fn count_wins((winners, scratchcard): (Vec<&str>, Vec<&str>))-> Result<usize> {
-        Ok(scratchcard.iter().filter(|x| winners.contains(&x)).count())
+fn count_wins((winners, scratchcard): (Vec<&str>, Vec<&str>)) -> Result<usize> {
+    Ok(scratchcard.iter().filter(|x| winners.contains(&x)).count())
 }
 
 fn parse_card(card: &str) -> Result<(Vec<&str>, Vec<&str>)> {
-    let (winners, scratchcard) = card[card.find(":").unwrap()+2..].split_once(" | ").unwrap();
-    Ok((winners.split_whitespace().collect(), scratchcard.split_whitespace().collect()))
+    let (winners, scratchcard) = card[card.find(":").unwrap() + 2..]
+        .split_once(" | ")
+        .unwrap();
+    Ok((
+        winners.split_whitespace().collect(),
+        scratchcard.split_whitespace().collect(),
+    ))
 }
 
 fn part1(input: &str) -> Result<usize> {
     let mut sum: usize = 0;
     for line in input.lines() {
         let num_wins = count_wins(parse_card(line)?)? as u32;
-        if num_wins > 0 { sum += usize::pow(2, num_wins-1); }
+        if num_wins > 0 {
+            sum += usize::pow(2, num_wins - 1);
+        }
     }
     Ok(sum)
 }
@@ -31,12 +38,14 @@ fn part2(input: &str) -> Result<usize> {
     for line in input.lines() {
         num_wins.push(count_wins(parse_card(line)?)?);
     }
-    let mut dp: Vec<usize> = vec![0; num_wins.len()]; 
+    let mut dp: Vec<usize> = vec![1; num_wins.len()];
     for i in (0..num_wins.len()).rev() {
-        if num_wins[i] == 0 {
-            dp[i] = 1;
-        } else {
-            dp[i] = 1 + (i+1..i+num_wins[i]+1).into_iter().map(|x| dp[x]).sum::<usize>();
+        if num_wins[i] > 0 {
+            dp[i] = 1
+                + (i + 1..i + num_wins[i] + 1)
+                    .into_iter()
+                    .map(|x| dp[x])
+                    .sum::<usize>();
         }
     }
     Ok(dp.iter().sum())
