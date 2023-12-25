1b1a8298445c52a14b0df9bc83c04e3d9e7c0cec
Saketh Thota <saketht@umich.edu>
2023-12-24 21:12:50 -0600

    parse and setup for day-09

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,3 +1,34 @@
+use anyhow::Result;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn extrapolate(history: Vec<usize>) -> Result<usize> {
+    Ok(0)
+}
+
+fn part1(input: &str) -> Result<usize> {
+    let mut sum = 0;
+    for line in input.lines() {
+        let history = line
+            .split_whitespace()
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+        sum += extrapolate(history)?;
+    }
+    Ok(sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        let expected = 114;
+        let result = part1(include_str!("example.in"));
+        println!("expected: {:?}, received {:?}", expected, result);
+        assert_eq!(result.is_ok_and(|x| x == expected), true);
+    }
 }
